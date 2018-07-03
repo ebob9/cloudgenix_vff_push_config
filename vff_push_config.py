3ee172772925c8265f794c7f7dc8208d8a9df9b7
Aaron Edwards <aaron@cloudgenix.com>
2018-07-03 16:15:58 -0500

    Adding stub script incase pip install isn't convenient.

---
+++ vff_push_config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Stub script for vff_push_client
+Should be installed via pip, but can use this script if it happens to be missed or not easy to find in path.
+"""
+
+from cloudgenix_vff_push_config import go
+
+if __name__ == "__main__":
+    go()
